0f07902e6505521df913fd83267a5a9dbebacdb3
pf20aai <pf20aai@herts.ac.uk>
2023-08-11 12:15:28 +0100

    Adding weather data paths
    Here are the new paths for the webserver

--- code/weatherProjectHttpServer/weatherProjectHttpServer/Program.cs
+++ code/weatherProjectHttpServer/weatherProjectHttpServer/Program.cs
@@ -1,44 +1,36 @@
 ﻿using System.Net;
 using System.Text;
+using System.Text.Json;
 
 // todo: extract common elements into functoins
 // todo: make more c# like
 
-using var listener = new HttpListener();
-listener.Prefixes.Add("http://localhost:80/");
 
-listener.Start();
-
-Console.WriteLine("Listening on port 80...");
-
-while (true)
+class httpWebserver
 {
-    HttpListenerContext context = listener.GetContext();
-    HttpListenerRequest req = context.Request;
 
-    Console.WriteLine($"Received {req.HttpMethod} request to {req.Url.AbsolutePath} from {req.Url}");
-
-    if ((req.HttpMethod == "GET") && (req.Url.AbsolutePath == "/test"))
+    static void SendHttpResponse(HttpListenerContext context, int statusCode, string data = "")
     {
-        Console.WriteLine("Yeah that works");
-
         using HttpListenerResponse resp = context.Response;
         resp.Headers.Set("Content-Type", "text/plain");
 
-        string data = "Hello there!";
-        resp.StatusCode = (int) HttpStatusCode.OK;
+        resp.StatusCode = statusCode;
 
         byte[] buffer = Encoding.UTF8.GetBytes(data);
-
         resp.ContentLength64 = buffer.Length;
         using Stream ros = resp.OutputStream;
-        ros.Write(buffer, 0, buffer.Length);
 
+        ros.Write(buffer, 0, buffer.Length);
         resp.Close();
     }
 
+    static void SendNotFoundResponse(HttpListenerContext context)
+    {
+        Console.WriteLine("Unknown path");
+        SendHttpResponse(context, (int)HttpStatusCode.NotFound, "Not Found");
+    }
 
-    else if ((req.HttpMethod == "POST") && (req.Url.AbsolutePath == "/test"))
+    static string GetRequestData(HttpListenerRequest req)
     {
         System.IO.Stream body = req.InputStream;
         System.Text.Encoding encoding = req.ContentEncoding;
@@ -49,41 +41,187 @@ while (true)
         reader.Close();
 
 
-        Console.WriteLine($"Recieved Data:\n{requestData}");
-
-
-        using HttpListenerResponse resp = context.Response;
-        resp.Headers.Set("Content-Type", "text/plain");
-
-        string responseData = $"Accepted data:\n{requestData}";
-        resp.StatusCode = (int)HttpStatusCode.Accepted;
-
-        byte[] buffer = Encoding.UTF8.GetBytes(responseData);
-        resp.ContentLength64 = buffer.Length;
-        using Stream ros = resp.OutputStream;
-        ros.Write(buffer, 0, buffer.Length);
-
-        resp.Close();
+        return (requestData);
     }
 
-    else
+    static bool AttemptLogin(string details)
     {
-        Console.WriteLine("Unknown path");
-
-        using HttpListenerResponse resp = context.Response;
-        resp.Headers.Set("Content-Type", "text/plain");
-
-        string data = "Not Found";
-        resp.StatusCode= (int) HttpStatusCode.NotFound;
-
-        byte[] buffer = Encoding.UTF8.GetBytes(data);
-        resp.ContentLength64 = buffer.Length;
-        using Stream ros = resp.OutputStream;
-
-        ros.Write(buffer, 0, buffer.Length);
-        resp.Close();
-
+        if (details == "secretTunnel")
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
-    
+    static string SignUpUser(string data)
+    {
+        if (data == "fight")
+        {
+            return ("conflict");
+        }
+        else
+        {
+            return ("123456789");
+        }
+    }
+
+    static string GeneratePrediction(string data)
+    {
+        string prediction = "IDK cloudy I guess?";
+
+        return (prediction);
+    }
+
+    static void SendDataToDatabase(string data)
+    {
+        Console.WriteLine($"Sending data to database\n{data}");
+    }
+
+    static void UpdateDataInDatabase(string data, string entryId)
+    {
+        Console.WriteLine($"Updating data for entry {entryId} in database\n{data}");
+    }
+
+    static string RetrieveWeatherData(string county = "")
+    {
+        var countys = new List<string>()
+        {
+            "devon",
+            "cornwall"
+        };
+        if (county != "")
+        {
+            if (countys.Contains(county.ToLower()))
+            {
+                Console.WriteLine($"Get data for {county}");
+                return ($"Data for {county}");
+            }
+            else
+            {
+                Console.WriteLine($"Couldn't find {county}");
+                return ("not found");
+            }
+        }
+        else
+        {
+            Console.WriteLine("Get weather data");
+            return ("Weather data here");
+        }
+    }
+
+    static void Main()
+    {
+        using var listener = new HttpListener();
+        listener.Prefixes.Add("http://localhost:80/");
+
+        listener.Start();
+
+        Console.WriteLine("Listening on port 80...");
+
+        while (true)
+        {
+            HttpListenerContext context = listener.GetContext();
+            HttpListenerRequest req = context.Request;
+
+            Console.WriteLine($"Received {req.HttpMethod} request to {req.Url.AbsolutePath} from {req.Url}");
+
+            if (req.HttpMethod == "GET")
+            {
+                if (req.Url.AbsolutePath == "/weather-data")
+                {
+                    string data = RetrieveWeatherData();
+                    SendHttpResponse(context, (int)HttpStatusCode.OK, data);
+                }
+                else if (req.Url.AbsolutePath == "/weather-data/county")
+                {
+                    string county = req.QueryString["county"];
+                    string data = RetrieveWeatherData(county);
+
+                    SendHttpResponse(context, (int)HttpStatusCode.OK, data);
+                }
+
+                else
+                {
+                    SendNotFoundResponse(context);
+                }
+            }
+            else if (req.HttpMethod == "POST")
+            {
+                if (req.Url.AbsolutePath == "/login")
+                {
+                    string data = GetRequestData(req);
+                    
+                    bool authenticated = AttemptLogin(data);
+
+                    if (authenticated)
+                    {
+                        SendHttpResponse(context, (int)HttpStatusCode.OK, "{id: abcd12345}");
+                    }
+                    else
+                    {
+                        SendHttpResponse(context, (int)HttpStatusCode.Unauthorized, "Unknown user");
+                    }
+                    
+                }
+                else if (req.Url.AbsolutePath == "/signup")
+                {
+                    string signUpRequest = GetRequestData(req);
+
+                    string signUpResult = SignUpUser(signUpRequest);
+
+                    if (signUpResult == "conflict")
+                    {
+                        SendHttpResponse(context, (int)HttpStatusCode.Conflict, "userConflict");
+                    }
+                    else
+                    {
+                        SendHttpResponse(context, (int)HttpStatusCode.Accepted, signUpResult);
+                    }
+
+                }
+                // put something here to make sure everything is authenticated
+                else if (req.Url.AbsolutePath == "/predict")
+                {
+                    string data = GetRequestData(req);
+                    string prediction = GeneratePrediction(data);
+
+                    SendHttpResponse(context, (int)HttpStatusCode.OK, prediction);
+                }
+                else if (req.Url.AbsolutePath == "/weather-data")  //need admin auth here
+                {
+                    string data = GetRequestData(req);
+                    SendDataToDatabase(data);
+                    SendHttpResponse(context, (int)HttpStatusCode.Created);
+                }
+                else
+                {
+                    SendNotFoundResponse(context);
+                }
+            }
+            else if (req.HttpMethod == "PUT")
+            {
+                if (req.Url.AbsolutePath == "weather-data")
+                {
+                    string data = GetRequestData(req);
+                    UpdateDataInDatabase(data, "1234567890");
+                    SendHttpResponse(context, (int)HttpStatusCode.Accepted);
+                }
+                else
+                {
+                    SendNotFoundResponse(context);
+                }
+            }
+
+            else
+            {
+                SendNotFoundResponse(context);
+            }
+
+        }
+    }
 }
+
+
